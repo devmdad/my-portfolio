21a847bc960d89408787378e94b3cfeed39e36d4
abc <abc@example.com>
2024-07-25 04:19:44 +0500

    removed conflicts

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 export const educations = [
   {
     id: 1,
@@ -18,25 +18,5 @@ export const educations = [
     duration: "2016 - 2017",
     institution: "City Public High School",
   }
-=======
-export const educations = [
-  {
-    id: 1,
-    title: "Bachelor in Software Engineering",
-    duration: "2020 - 2024",
-    institution: "University of Sialkot",
-  },
-  {
-    id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Murray College",
-  },
-  {
-    id: 3,
-    title: "Secondary School Certificate",
-    duration: "2016 - 2017",
-    institution: "City Public High School",
-  }
->>>>>>> origin/main
+
 ]
