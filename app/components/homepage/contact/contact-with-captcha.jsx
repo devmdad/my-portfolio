2d75f4c4964b6a121bc0ce4be1c6b0e907f85999
abc <abc@example.com>
2024-07-25 16:18:17 +0500

    fixed button disabled

--- app/components/homepage/contact/contact-with-captcha.jsx
+++ app/components/homepage/contact/contact-with-captcha.jsx
@@ -1,24 +1,26 @@
 "use client";
 // @flow strict
-import { isValidEmail } from '@/utils/check-email';
-import emailjs from '@emailjs/browser';
-import axios from 'axios';
-import { useState } from 'react';
-import ReCAPTCHA from 'react-google-recaptcha';
+import { isValidEmail } from "@/utils/check-email";
+import emailjs from "@emailjs/browser";
+import axios from "axios";
+import { useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 import { TbMailForward } from "react-icons/tb";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
 function ContactWithCaptcha() {
   const [input, setInput] = useState({
-    from_name: '',
-    email: '',
-    message: '',
+    from_name: "",
+    email: "",
+    message: "",
+    reply_to: input.email,
   });
   const [captcha, setCaptcha] = useState(null);
   const [error, setError] = useState({
     email: false,
     required: false,
   });
+  const [loading, setLoading] = useState(false);
 
   const checkRequired = () => {
     if (input.email && input.message && input.from_name) {
@@ -29,9 +31,9 @@ function ContactWithCaptcha() {
   const handleSendMail = async (e) => {
     e.preventDefault();
     if (!captcha) {
-      toast.error('Please complete the captcha!');
+      toast.error("Please complete the captcha!");
       return;
-    };
+    }
 
     if (!input.email || !input.message || !input.from_name) {
       setError({ ...error, required: true });
@@ -40,28 +42,37 @@ function ContactWithCaptcha() {
       return;
     } else {
       setError({ ...error, required: false });
-    };
+    }
 
     const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
     const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
     const options = { publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY };
 
+    setInput({ ...input, reply_to: input.email });
+
     try {
+      setLoading(true);
       const res = await emailjs.send(serviceID, templateID, input, options);
-      const teleRes = await axios.post(`${process.env.NEXT_PUBLIC_APP_URL}/api/contact`, input);
+      const teleRes = await axios.post(
+        `${process.env.NEXT_PUBLIC_APP_URL}/api/contact`,
+        input
+      );
 
       if (res.status === 200 || teleRes.status === 200) {
-        toast.success('Message sent successfully!');
+        setLoading(false);
+        toast.success("Message sent successfully!");
         setInput({
-          from_name: '',
-          email: '',
-          message: '',
+          from_name: "",
+          email: "",
+          message: "",
+          reply_to: "",
         });
         setCaptcha(null);
-      };
+      }
     } catch (error) {
+      setLoading(false);
       toast.error(error?.text || error);
-    };
+    }
   };
 
   return (
@@ -71,7 +82,9 @@ function ContactWithCaptcha() {
       </p>
       <div className="max-w-3xl text-white rounded-lg border border-[#464c6a] p-3 lg:p-5">
         <p className="text-sm text-[#d3d8e8]">
-          {"If you have any questions or concerns, please don't hesitate to contact me. I am open to any work opportunities that align with my skills and interests."}
+          {
+            "If you have any questions or concerns, please don't hesitate to contact me. I am open to any work opportunities that align with my skills and interests."
+          }
         </p>
         <div className="mt-6 flex flex-col gap-4">
           <div className="flex flex-col gap-2">
@@ -81,7 +94,9 @@ function ContactWithCaptcha() {
               type="text"
               maxLength="100"
               required={true}
-              onChange={(e) => setInput({ ...input, from_name: e.target.value })}
+              onChange={(e) =>
+                setInput({ ...input, from_name: e.target.value })
+              }
               onBlur={checkRequired}
               value={input.from_name}
             />
@@ -101,9 +116,11 @@ function ContactWithCaptcha() {
                 setError({ ...error, email: !isValidEmail(input.email) });
               }}
             />
-            {error.email &&
-              <p className="text-sm text-red-400">Please provide a valid email!</p>
-            }
+            {error.email && (
+              <p className="text-sm text-red-400">
+                Please provide a valid email!
+              </p>
+            )}
           </div>
 
           <div className="flex flex-col gap-2">
@@ -124,15 +141,16 @@ function ContactWithCaptcha() {
             onChange={(code) => setCaptcha(code)}
           />
           <div className="flex flex-col items-center gap-2">
-            {error.required &&
+            {error.required && (
               <p className="text-sm text-red-400">
                 Email and Message are required!
               </p>
-            }
+            )}
             <button
               className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-5 md:px-12 py-2.5 md:py-3 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
               role="button"
               onClick={handleSendMail}
+              disabled={loading}
             >
               <span>Send Message</span>
               <TbMailForward className="mt-1" size={18} />
@@ -142,6 +160,6 @@ function ContactWithCaptcha() {
       </div>
     </div>
   );
-};
+}
 
 export default ContactWithCaptcha;
